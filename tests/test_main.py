5db8507760296df218f7d937b5408079eb848b6a
Aleksey Belyaev <lesha.belyaev.73@mail.ru>
2023-11-06 18:15:10 +0300

    Выполнил тестирование

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,10 @@
-def test_my_slice()
+from src.main import my_slice
+
+
+def test_my_slice():
+    assert my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
+    assert my_slice([1, 2, 3, 4], -1, 3) == []
+    assert my_slice([], 1, 3) == []
+    assert my_slice([2, 3], -5, -3) == []
+
 
